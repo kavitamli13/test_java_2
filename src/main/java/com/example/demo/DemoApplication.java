35a6e40a8788bb5c113da621daafc70871927780
kavitamli13 <kavithanjalitamilselvan@gmail.com>
2025-12-16 16:09:35 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -13,7 +13,17 @@ public class DemoApplication {
     public String hello() {
         return "Hello from Kubernetes Latestt!";
     }
+	
+	@GetMapping("/test_app")
+    public String test_app() {
+        return "Hello from Test App!";
+	}
 
+	@PostMapping("/test_app2")
+    public String test_app2(String test_str) {
+        return "Hello from Test App! "+test_str;
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
